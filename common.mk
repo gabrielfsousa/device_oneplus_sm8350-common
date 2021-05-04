fc94ec6df79fc21bb7fd9178e477473d60a5b8ec
P.Adarsh Reddy <padarshr@codeaurora.org>
2021-05-04 15:29:23 +0530

    sm8350-common: Enforce android-S new launch debugfs restrictions
    Change-Id: I78878e1c4177c07d66b13e5659d4360252c5f87b

--- common.mk
+++ common.mk
@@ -132,6 +132,9 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk_platform.vendor \
